e9573fc35d2fbac97a55db947cf11909274f6e4d
فاطمه نورائی <f.nouraei@dybug.ir>
2026-09-01 17:08:05 +0330

    4. Using markup to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,20 @@
-/** @owl-module */
+/** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.Playground";
     static components = { Counter, Card };
+
+    setup() {
+        this.state = useState({ value: 0 });
+        this.html = markup("<div><b>Bold</b> html content</div>");
+        this.plainText = "<div>this will show as plain text</div>";
+    }
+
+    increment() {
+        this.state.value++;
+    }
 }
