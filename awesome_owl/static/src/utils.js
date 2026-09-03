79548219f82893cdead4246be582f1a658210666
فاطمه نورائی <f.nouraei@dybug.ir>
2026-09-03 14:29:02 +0330

    10. Focusing the input

---
+++ awesome_owl/static/src/utils.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { useRef, onMounted } from "@odoo/owl";
+
+export function useAutofocus() {
+    const inputRef = useRef("input");
+
+    onMounted(() => {
+        inputRef.el.focus();
+    });
+
+    return inputRef;
+}
