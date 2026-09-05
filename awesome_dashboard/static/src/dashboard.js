225933e88ee88883d718f67e14f5d53ef56f4a32
فاطمه نورائی <f.nouraei@dybug.ir>
2026-09-05 17:28:05 +0330

    4. Call the server, add some statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,28 +1,36 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./DashboardItem/dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-
-    static components = {
-        Layout,DashboardItem
-    };
-
-    static props = {
-        action: Object,
-    };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
+
+
+        this.statistics = useState({
+            average_amount: 0,
+            average_time: 0,
+            nb_new_orders: 0,
+            nb_cancelled_orders: 0,
+            total_amount: 0,
+        });
+
+        onWillStart(async () => {
+            const result = await rpc("/awesome_dashboard/statistics");
+            Object.assign(this.statistics, result);
+        });
     }
 
     openCustomers() {
-        this.action.doAction("base.action_partner_form");
+        this.action.doAction("base.action_partner_formfields");
     }
 
     openLeads() {
