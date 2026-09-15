0ae15d192daab7eacdb89d1ab28b9fd21fbdd60b
فاطمه نورائی <f.nouraei@dybug.ir>
2026-09-15 10:08:10 +0330

    9. Switch to form view on click

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
 import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { url } from "@web/core/utils/urls";
 
 export class GalleryRenderer extends Component {
     static template = "awesome_gallery.GalleryRenderer";
+
+    setup() {
+        this.action = useService("action");
+    }
 
     getImageUrl(image) {
         return url("/web/image", {
@@ -13,4 +18,10 @@ export class GalleryRenderer extends Component {
             field: this.props.imageField,
         });
     }
+
+    openRecord = (image) => {
+        this.action.switchView("form", {
+            resId: image.id,
+        });
+    };
 }
