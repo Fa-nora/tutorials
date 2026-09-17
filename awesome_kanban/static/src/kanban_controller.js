d3e77be4f618b42f11f80cde9011bd60073dd6ae
فاطمه نورائی <f.nouraei@dybug.ir>
2026-09-17 11:51:13 +0330

    4. Update the main kanban view

--- awesome_kanban/static/src/kanban_controller.js
+++ awesome_kanban/static/src/kanban_controller.js
@@ -4,8 +4,33 @@ import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { CustomerList } from "./customer_list";
 
 export class AwesomeKanbanController extends KanbanController {
-    static components = {
+    static template = "awesome_kanban.AwesomeKanbanView";
+
+	static components = {
         ...KanbanController.components,
         CustomerList,
     };
+
+    selectCustomer(partner) {
+        // پیدا کردن فیلترهای ساخته‌شده توسط CustomerList
+        const customerFilters = this.env.searchModel.getSearchItems(
+            (searchItem) => searchItem.isFromAwesomeKanban
+        );
+
+        // غیرفعال کردن فیلتر قبلی
+        for (const customerFilter of customerFilters) {
+            if (customerFilter.isActive) {
+                this.env.searchModel.toggleSearchItem(customerFilter.id);
+            }
+        }
+
+        // ساخت فیلتر جدید
+        this.env.searchModel.createNewFilters([
+            {
+                description: partner.name,
+                domain: [["partner_id", "=", partner.id]],
+                isFromAwesomeKanban: true,
+            },
+        ]);
+    }
 }
